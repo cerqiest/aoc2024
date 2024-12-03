526871e2d6b0a72457f081545087b1485756d08f
cerq <66092565+cerqiest@users.noreply.github.com>
2024-12-03 17:10:14 -0500

    day 2

---
+++ 2/a/Program.cs
@@ -0,0 +1,47 @@
+﻿var input = File.ReadLines("../../../input.txt");
+var safeReports = 0;
+
+foreach (var line in input)
+{
+    var levels = line.Split(" ");
+    var safeReport = true;
+    bool? increasing = null;
+    int? lastLevel = null;
+
+    foreach (var level in levels)
+    {
+        var levelInt = int.Parse(level);
+        
+        if (lastLevel.HasValue)
+        {
+            increasing ??= levelInt > lastLevel;
+            
+            if ((increasing ?? true) && levelInt <= lastLevel)
+            {
+                safeReport = false;
+                break;
+            }
+            
+            if (!(increasing ?? true) && levelInt >= lastLevel)
+            {
+                safeReport = false;
+                break;
+            }
+            
+            if (Math.Abs(levelInt - lastLevel.Value) > 3)
+            {
+                safeReport = false;
+                break;
+            }
+        }
+        
+        lastLevel = levelInt;
+    }
+    
+    if (safeReport)
+    {
+        safeReports++;
+    }
+}
+
+Console.WriteLine(safeReports);
---
+++ 2/b/Program.cs
@@ -0,0 +1,69 @@
+﻿var input = File.ReadLines("../../../input.txt");
+var safeReports = 0;
+
+bool ReportIsSafe(List<string> levels)
+{
+    bool? increasing = null;
+    int? lastLevel = null;
+
+    foreach (var level in levels)
+    {
+        var levelInt = int.Parse(level);
+        
+        if (lastLevel.HasValue)
+        {
+            increasing ??= levelInt > lastLevel;
+            
+            if ((increasing ?? true) && levelInt <= lastLevel)
+            {
+                return false;
+            }
+            
+            if (!(increasing ?? true) && levelInt >= lastLevel)
+            {
+                return false;
+            }
+            
+            if (Math.Abs(levelInt - lastLevel.Value) > 3)
+            {
+                return false;
+            }
+        }
+        
+        lastLevel = levelInt;
+    }
+
+    return true;
+}
+
+bool WouldBeDampened(List<string> levels)
+{
+    for (int i = 0; i < levels.Count; i++)
+    {
+        var levelsCopy = new List<string>(levels);
+        levelsCopy.RemoveAt(i);
+        if (ReportIsSafe(levelsCopy))
+        {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+foreach (var line in input)
+{
+    var levels = line.Split(" ").ToList(); 
+    if (ReportIsSafe(levels))
+    {
+        safeReports++;
+        continue;
+    }
+    
+    if (WouldBeDampened(levels))
+    {
+        safeReports++;
+    }
+}
+
+Console.WriteLine(safeReports);
